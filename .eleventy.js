3c48e9e768fbf8646963bd62702f03b543cda449
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-07-02 13:16:40 +0800

    Add 11ty plugin for using svg sprite

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
+const svgSprite = require('eleventy-plugin-svg-sprite')
 module.exports = (config) => {
-  config.addPassthroughCopy('src/images')
+  config.addPassthroughCopy('src/assets')
+
+  // Inline SVGs as a sprite
+  config.addPlugin(svgSprite, {
+    path: 'src/assets/svg',
+    globalClasses: 'icon',
+  })
+
+  config.addShortcode('icon', (name, customClass) => {
+    return `<svg aria-hidden="true" class="icon ${customClass}"><use href="#svg-${name}"></use></svg>`
+  })
 
   return {
     markdownTemplateEngine: 'njk',
